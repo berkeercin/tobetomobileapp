34c017d3b1923510cbfca1b8d6161c205c60c215
berkeercinr <berkeercin@outlook.com.tr>
2023-12-28 00:21:19 +0300

    ui tasarımları güncellendi

--- lib/widgets/swing.dart
+++ lib/widgets/swing.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:tobetomobileapp/constants/tobeto_colors.dart';
 
 class Swing extends StatefulWidget {
@@ -61,12 +62,37 @@ class swing_metod extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Swing(
-        button: FloatingActionButton(
+        button: SpeedDial(
       shape: CircleBorder(),
       backgroundColor: TobetoColor().boxButton,
       foregroundColor: Colors.white,
-      onPressed: () {},
+      elevation: 0,
+      overlayOpacity: 0,
+      childPadding: EdgeInsets.all(8),
       child: Icon(Icons.chat_bubble_rounded),
+      children: [
+        SpeedDialChild(
+          backgroundColor: TobetoColor().boxButton,
+          child: Icon(
+            Icons.chat_outlined,
+            color: Colors.white,
+          ),
+          onTap: () {},
+        ),
+        SpeedDialChild(
+            child: Icon(
+              Icons.chat,
+              color: Colors.white,
+            ),
+            backgroundColor: Colors.green)
+      ],
     ));
   }
 }
+//  FloatingActionButton(
+//       shape: CircleBorder(),
+//       backgroundColor: TobetoColor().boxButton,
+//       foregroundColor: Colors.white,
+//       onPressed: () {},
+//       child: Icon(Icons.chat_bubble_rounded),
+//     )
